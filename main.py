687c1341605bccc8cc10610aa845e88c8c4bb3bd
jmezar7 <joenmero@gmail.com>
2026-09-20 14:06:23 -0500

    modificacion(main.py): se cambio el nombre a dos empleados

--- main.py
+++ main.py
@@ -18,14 +18,14 @@ def main():
     # 1. Instanciación de empleados según las reglas del caso de estudio
     asalariado = EmpleadoAsalariado(
         identificacion="CC-101",
-        nombre="Laura Gómez",
+        nombre="Jorge Meza",
         anios_antiguedad=6,       # Aplica bono 10% por tener > 5 años
         salario_fijo=3_000_000.0
     )
 
     horas = EmpleadoPorHoras(
         identificacion="CC-102",
-        nombre="Pedro Martínez",
+        nombre="Ureliano Peñata",
         anios_antiguedad=2,       # Aplica a fondo de ahorro por > 1 año
         horas_trabajadas=48.0,    # 40 normales + 8 extras (x1.5)
         tarifa_hora=20_000.0,
